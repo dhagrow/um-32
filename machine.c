54d0a79d2afc2f5b3b58551d963dae5030d524ef
miguel <cymrow@gmail.com>
2018-05-02 14:06:00 -0400

    machine.c: endianess

--- machine.c
+++ machine.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <byteswap.h>
 
 uint32_t **memory;
 uint32_t *array_sizes;
 uint32_t finger;
+
+void cycle() {
+    printf("platters: %u\n", array_sizes[0]);
+
+    uint32_t platter;
+    finger = 0;
+
+    while (finger < array_sizes[0]) {
+        platter = memory[0][finger];
+        printf("platter: %u\n", __bswap_32(platter));
+        finger++;
+    }
+}
 
 uint32_t load(FILE *fp, uint32_t **array) {
     fseek(fp, 0L, SEEK_END);
@@ -22,23 +36,6 @@ uint32_t load(FILE *fp, uint32_t **array) {
     }
 
     return num_platters;
-}
-
-void cycle() {
-    printf("platters: %u\n", array_sizes[0]);
-
-    uint32_t platter;
-    finger = 0;
-
-    while (finger < array_sizes[0]) {
-        printf("finger: %u\n", finger);
-        if (finger == 10) {
-            break;
-        }
-        platter = memory[0][finger];
-        printf("platter: %u\n", platter);
-        finger++;
-    }
 }
 
 int main(void) {
