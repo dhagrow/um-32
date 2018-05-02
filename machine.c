ba882909a303e77b3235daf1566ffbb843fedfc4
miguel <cymrow@gmail.com>
2018-05-02 10:34:30 -0400

    machine.c: initial cycle

--- machine.c
+++ machine.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 uint32_t **memory;
+uint32_t *array_sizes;
 
 uint32_t *load(FILE *fp) {
     fseek(fp, 0L, SEEK_END);
@@ -19,17 +20,31 @@ uint32_t *load(FILE *fp) {
         bytes_read = fread(pos, sizeof(*pos), 1, fp);
     }
 
-    printf("pos: %d", pos - array);
+    printf("pos: %li\n", pos - array);
 
     return array;
 }
 
+void cycle() {
+    for (int i = 0; i < 10; i++) {
+        printf("cycle: %d\n", i);
+    }
+}
+
 int main(void) {
+    // initialize array 0
+    memory = (uint32_t **) malloc(sizeof(uint32_t));
+    array_sizes = (uint32_t *) malloc(sizeof(uint32_t));
+
     FILE *fp = fopen("scrolls/sandmark.umz", "rb");
-    uint32_t *array = load(fp);
+    memory[0] = load(fp);
     fclose(fp);
 
-    free(array);
+    cycle();
+
+    free(memory[0]);
+    free(array_sizes);
+    free(memory);
 
     return 0;
 }
