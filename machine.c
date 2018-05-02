2534b5fdded026214349ba771f66f1ae315603d9
miguel <cymrow@gmail.com>
2018-05-02 13:59:23 -0400

    machine.c: cycler no cycling!

--- machine.c
+++ machine.c
@@ -4,30 +4,40 @@
 
 uint32_t **memory;
 uint32_t *array_sizes;
+uint32_t finger;
 
-uint32_t *load(FILE *fp) {
+uint32_t load(FILE *fp, uint32_t **array) {
     fseek(fp, 0L, SEEK_END);
     long size = ftell(fp);
     rewind(fp);
 
     uint32_t num_platters = size / sizeof(uint32_t);
-    uint32_t *array = (uint32_t *) malloc(num_platters * sizeof(uint32_t));
+    *array = (uint32_t *) malloc(num_platters * sizeof(uint32_t));
 
-    uint32_t *pos = array;
+    uint32_t *pos = *array;
     size_t bytes_read = fread(pos, sizeof(*pos), 1, fp);
     while (bytes_read != 0) {
         ++pos;
         bytes_read = fread(pos, sizeof(*pos), 1, fp);
     }
 
-    printf("pos: %li\n", pos - array);
-
-    return array;
+    return num_platters;
 }
 
 void cycle() {
-    for (int i = 0; i < 10; i++) {
-        printf("cycle: %d\n", i);
+    printf("platters: %u\n", array_sizes[0]);
+
+    uint32_t platter;
+    finger = 0;
+
+    while (finger < array_sizes[0]) {
+        printf("finger: %u\n", finger);
+        if (finger == 10) {
+            break;
+        }
+        platter = memory[0][finger];
+        printf("platter: %u\n", platter);
+        finger++;
     }
 }
 
@@ -37,7 +47,7 @@ int main(void) {
     array_sizes = (uint32_t *) malloc(sizeof(uint32_t));
 
     FILE *fp = fopen("scrolls/sandmark.umz", "rb");
-    memory[0] = load(fp);
+    array_sizes[0] = load(fp, &(memory[0]));
     fclose(fp);
 
     cycle();
