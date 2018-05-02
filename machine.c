fa49cfa224bddae9bbc60191d1682fd874f48a73
dhagrow <cymrow@gmail.com>
2018-05-02 07:50:07 -0400

    machine.c: load

--- machine.c
+++ machine.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-uint32_t *memory;
+uint32_t **memory;
+
+uint32_t *load(FILE *fp) {
+    fseek(fp, 0L, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+
+    uint32_t num_platters = size / sizeof(uint32_t);
+    uint32_t *array = (uint32_t *) malloc(num_platters * sizeof(uint32_t));
+
+    uint32_t *pos = array;
+    size_t bytes_read = fread(pos, sizeof(*pos), 1, fp);
+    while (bytes_read != 0) {
+        ++pos;
+        bytes_read = fread(pos, sizeof(*pos), 1, fp);
+    }
+
+    printf("pos: %d", pos - array);
+
+    return array;
+}
 
 int main(void) {
-    size_t bytes_read;
-    uint32_t platter;
-
     FILE *fp = fopen("scrolls/sandmark.umz", "rb");
-    bytes_read = fread(&platter, sizeof platter, 1, fp);
-
-    printf("read: %d platter: %u", bytes_read, platter);
-
+    uint32_t *array = load(fp);
     fclose(fp);
+
+    free(array);
 
     return 0;
 }
