5c0eca21c24c78d61aee82ba44b76d62c5b27f44
dhagrow <cymrow@gmail.com>
2018-05-06 01:06:54 -0400

    - holy shit it works - memory now and array of structs - allocate/abandon implemented - we do have a memleak

--- machine.c
+++ machine.c
@@ -5,15 +5,43 @@
 #include <signal.h>
 #include <byteswap.h>
 
+typedef struct {
+    uint32_t *data;
+    uint32_t size;
+} array;
+
 uint8_t volatile stop = 0;
-uint32_t **memory;
-uint32_t *array_sizes;
+array *memory;
+uint32_t mem_size;
 uint32_t cycle;
 uint32_t finger;
 uint32_t reg[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 enum Operators {
     cmv, aix, aam, add, mul, dvi, nad, hlt, alc, abd, out, inp, lod, ort};
+
+uint32_t new_array(uint32_t size) {
+    uint32_t index;
+    array *tmp = realloc(memory, (mem_size + 1) * sizeof(*tmp));
+
+    if (!tmp) {
+        // fail
+    } else {
+        index = mem_size;
+        mem_size++;
+        memory = tmp;
+
+        memory[index].data = calloc(size, sizeof(uint32_t));
+        memory[index].size = size;
+    }
+
+    return index;
+}
+
+uint32_t free_array(uint32_t index) {
+    free(memory[index].data);
+    memory[index].data = 0;
+}
 
 void state(uint8_t code, uint8_t a, uint8_t b, uint8_t c) {
     printf("\nSTOP at %u (cycle %u)\n", finger, cycle);
@@ -23,7 +51,7 @@ void state(uint8_t code, uint8_t a, uint8_t b, uint8_t c) {
         printf("%u, ", reg[i]);
     }
     printf("%u]\n", reg[7]);
-    printf("memory {0: %u}\n", array_sizes[0]);
+    printf("memory {0: %u}\n", memory[0].size);
 }
 
 void run(uint32_t limit) {
@@ -37,8 +65,8 @@ void run(uint32_t limit) {
     cycle = 0;
     finger = 0;
 
-    while (!stop && finger < array_sizes[0]) {
-        platter = memory[0][finger];
+    while (!stop && finger < memory[0].size) {
+        platter = memory[0].data[finger];
         code = (platter >> 28);
 
         if (code == ort) {
@@ -57,10 +85,10 @@ void run(uint32_t limit) {
             }
             break;
         case aix: // 01
-            reg[a] = memory[reg[b]][reg[c]];
+            reg[a] = memory[reg[b]].data[reg[c]];
             break;
         case aam: // 02
-            memory[reg[a]][reg[b]] = reg[c];
+            memory[reg[a]].data[reg[b]] = reg[c];
             break;
         case add: // 03
             reg[a] = (reg[b] + reg[c]) % (1UL << 32);
@@ -74,22 +102,23 @@ void run(uint32_t limit) {
         case nad: // 06
             reg[a] = (reg[b] & reg[c]) ^ ((1UL << 32) - 1);
             break;
-        // case alc: // 08
-        //     reg[b] = new_array(reg[c]);
-        //     break;
-        // case abd: // 09
-        //     free((void *) reg[c]);
-        //     break;
+        case alc: // 08
+            reg[b] = new_array(reg[c]);
+            break;
+        case abd: // 09
+            free_array(reg[c]);
+            break;
         case out: // 10
             putchar(reg[c]);
             break;
         case lod: // 12
             if (reg[b] != 0) {
-                free(memory[0]);
-                val = array_sizes[reg[b]]; // size
-                memory[0] = (uint32_t *) malloc(val * sizeof(uint32_t));
-                memcpy(memory[0], memory[reg[b]], val * sizeof(uint32_t));
-                array_sizes[0] = val;
+                free(memory[0].data);
+                val = memory[reg[b]].size;
+                memory[0].data = malloc(val * sizeof(uint32_t));
+                memory[0].size = val;
+                memcpy(memory[0].data, memory[reg[b]].data,
+                    val * sizeof(uint32_t));
             }
             finger = reg[c] - 1;
             break;
@@ -116,15 +145,15 @@ void run(uint32_t limit) {
     }
 }
 
-uint32_t load(FILE *fp, uint32_t **array) {
+void load(FILE *fp, array *arr) {
     fseek(fp, 0L, SEEK_END);
     long size = ftell(fp);
     rewind(fp);
 
-    uint32_t num_platters = size / sizeof(uint32_t);
-    *array = (uint32_t *) malloc(num_platters * sizeof(uint32_t));
+    arr->data = malloc(size);
+    arr->size = size / sizeof(uint32_t);
 
-    uint32_t *pos = *array;
+    uint32_t *pos = arr->data;
     size_t bytes_read;
     while (1) {
         bytes_read = fread(pos, sizeof(*pos), 1, fp);
@@ -132,8 +161,6 @@ uint32_t load(FILE *fp, uint32_t **array) {
         *pos = __bswap_32(*pos);
         ++pos;
     }
-
-    return num_platters;
 }
 
 void sig_handler(int sig) {
@@ -146,17 +173,16 @@ int main(int argc, char *argv[]) {
     uint32_t limit = argc == 2 ? atoi(argv[1]) : 0;
 
     // initialize array 0
-    memory = (uint32_t **) malloc(sizeof(uint32_t *));
-    array_sizes = (uint32_t *) malloc(sizeof(uint32_t));
+    memory = malloc(sizeof(*memory));
+    mem_size = 1;
 
     FILE *fp = fopen("scrolls/sandmark.umz", "rb");
-    array_sizes[0] = load(fp, &(memory[0]));
+    load(fp, &(memory[0]));
     fclose(fp);
 
     run(limit);
 
-    free(memory[0]);
-    free(array_sizes);
+    free(memory[0].data);
     free(memory);
 
     return 0;
