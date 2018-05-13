4e8dc82265dda0c865d8091e0b406d87f14bff6b
dhagrow <cymrow@gmail.com>
2018-05-13 09:43:54 -0400

    machine.fs: progress

--- machine.fs
+++ machine.fs
@@ -1,23 +1,29 @@
 open System.IO
 
-let batchSize = 10L
+let mutable memory = new ResizeArray<ResizeArray<uint32>>()
 
-let byteSeq (reader: BinaryReader) =
-    let bs = reader.BaseStream
-    seq {
-        printf "seq"
-        printf "%i %i" (bs.Position |> int) (bs.Length |> int)
-        while not (bs.Position = bs.Length) do // bs.Position is always 0 in this outer loop
-            printf "-> %i" (bs.Position |> int)
-            yield seq {
-                let sPos = bs.Position
-                printf "-> %i" (bs.Position |> int)
-                while not (bs.Position = (min (sPos + batchSize) bs.Length)) do // bs.Position is increasing as it reads the bytes
-                    yield reader.ReadByte()
-            }
-    }
+let load filename =
+    let mutable program = new ResizeArray<uint32>()
+    let fp = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read)
+    let reader = new BinaryReader(fp)
+    try
+        while true do
+            let byte = reader.ReadUInt32()
+            program.Add byte
+    with
+    | :? System.IO.EndOfStreamException -> ()
 
-let fileName = @"scrolls/sandmark.umz";
-let reader = new BinaryReader(File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+    memory.Add program
 
-byteSeq reader |> ignore
+let run () =
+    let finger = 0
+    let stop = false
+
+    printfn "len: %i" memory.Count
+
+    while not stop do
+        let platter = memory.[0].[finger]
+        printfn "platter: %u" platter
+
+load @"scrolls/sandmark.umz"
+run()
