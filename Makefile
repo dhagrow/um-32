412d0d06afe9adaee8db92614be3f5b3239eca3d
miguel <cymrow@gmail.com>
2018-05-14 10:16:24 -0400

    machine.fs: much progress

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
-all: machine
+all: machine-c machine-fs
 
-fsharp:
+machine-c: machine.c
+	gcc -o machine-c machine.c
+
+machine-fs: machine.fs
+	fsharpc -o machine-fs machine.fs
+
+machine-fs-docker:
 	fsharp bash -c "fsharpc machine.fs && mono ./machine.exe"
--- machine.fs
+++ machine.fs
@@ -25,15 +25,16 @@ let load filename =
     let reader = new BinaryReader(fp)
     try
         while true do
-            let byte = reader.ReadUInt32()
-            program.Add byte
+            let platter = reader.ReadInt32() |> System.Net.IPAddress.HostToNetworkOrder
+            program.Add (uint32 platter)
     with
     | :? System.IO.EndOfStreamException -> ()
 
     memory.Add program
 
 let run () =
-    let finger = ref 0
+    let mutable finger = 0ul
+    let mutable cycle = 0ul
     let mutable stop = false
 
     let mutable platter = 0ul
@@ -43,34 +44,46 @@ let run () =
     let mutable c = 0uy
     let mutable value = 0ul
 
-    while not stop && !finger < memory.[0].Count do
-        platter <- memory.[0].[!finger]
-        code <- uint8 platter >>> 28
-
-        printfn "code: %u" code
+    while not stop && (int finger) < memory.[0].Count do
+        // printfn "cycle: %u" cycle
+        platter <- memory.[0].[int finger]
+        code <- uint8 (platter >>> 28)
 
         match enum<Op>(int32 code) with
         | Op.ort ->
             a <- (uint8 (platter >>> 25)) &&& 7uy
             value <- platter &&& 0x1fffffful
+            // printfn "%u(%u, %u)" code a value
             reg.[int a] <- value
         | _ ->
             a <- (uint8 (platter >>> 6)) &&& 7uy
             b <- (uint8 (platter >>> 3)) &&& 7uy
             c <- (uint8 platter) &&& 7uy
 
+            // printfn "%u(%u, %u, %u)" code a b c
+            // printfn "reg: %A" reg
+
             match enum<Op>(int32 code) with
             | Op.cmv -> if reg.[int c] <> 0ul then reg.[int a] <- reg.[int b]
+            | Op.aix -> reg.[int a] <- memory.[int reg.[int b]].[int reg.[int c]]
+            | Op.aam -> memory.[int reg.[int a]].[int reg.[int b]] <- reg.[int c]
             | Op.add -> reg.[int a] <- reg.[int b] + reg.[int c]
+            | Op.mul -> reg.[int a] <- reg.[int b] * reg.[int c]
+            | Op.dvi -> reg.[int a] <- reg.[int b] / reg.[int c]
+            | Op.nad -> reg.[int a] <- ~~~(reg.[int b] &&& reg.[int c])
+            | Op.out ->
+                printf "%c" (char reg.[int c])
+                stdout.Flush()
             | Op.lod ->
                 if reg.[int b] <> 0ul then
                     memory.[0] <- new ResizeArray<uint32>(memory.[int reg.[int b]])
-                finger := reg.[int c]
+                finger <- reg.[int c] - 1ul
             | _ ->
                 printfn "unknown code: %u" code
                 stop <- true
 
-        incr finger
+        finger <- finger + 1ul
+        cycle <- cycle + 1ul
 
 load @"scrolls/sandmark.umz"
 run()
