4961cd2de5ebcdb1cbf8cae9ba90aad1b1ce6fbf
miguel <cymrow@gmail.com>
2018-05-15 13:39:22 -0400

    machine.nim: progress

--- machine.nim
+++ machine.nim
@@ -2,11 +2,38 @@ import endians
 import streams
 import strformat
 
-var fs = newFileStream("scrolls/sandmark.umz", fmRead)
-if not isNil(fs):
+var
+  memory = newSeq[seq[uint32]]()
+
+proc load(filename: string) =
+  var fs = newFileStream(filename, fmRead)
+  if isNil(fs):
+    quit(QuitFailure)
+
+  var program = newSeq[uint32]()
   var platter: uint32
   while not fs.atEnd:
     platter = fs.readUint32()
     bigEndian32(addr(platter), addr(platter))
-    echo &"{platter:032b}\n"
+    program.add(platter)
   fs.close()
+
+  memory.setLen(0)
+  memory.add(program)
+
+proc run() =
+  var
+    finger = 0'u32
+    cycle = 0'u32
+    stop = false
+
+    platter: uint32
+
+  while not stop and finger < (uint32 memory[0].len):
+    platter = memory[0][int finger]
+    echo platter
+    break
+
+when isMainModule:
+  load("scrolls/sandmark.umz")
+  run()
