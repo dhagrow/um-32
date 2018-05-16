d43b931e6d9179921ecec1444d1588011bda2b42
dhagrow <cymrow@gmail.com>
2018-05-15 23:51:37 -0400

    machine.nim: sandmark!

--- Nim/machine.nim
+++ Nim/machine.nim
@@ -8,6 +8,7 @@ type
 
 var
   memory = newSeq[seq[uint32]]()
+  abandoned_indexes = newSeq[uint32]()
   reg: array[0..7, uint32]
 
 proc load(filename: string) =
@@ -69,6 +70,20 @@ proc run() =
       of ord(dvi): reg[a] = reg[b] div reg[c]
       of ord(nad): reg[a] = not (reg[b] and reg[c])
       of ord(hlt): stop = true
+      of ord(alc):
+        var index = 0'u32
+        let new_seq = newSeq[uint32](reg[c])
+        if abandoned_indexes.len > 0:
+          index = abandoned_indexes[0]
+          abandoned_indexes.del(0)
+          memory[int index] = new_seq
+        else:
+          index = uint32 memory.len
+          memory.add(new_seq)
+        reg[b] = index
+      of ord(abd):
+        memory[int reg[c]].setLen(0)
+        abandoned_indexes.add(reg[c])
       of ord(otp):
         stdout.write(char reg[c])
         flushFile(stdout)
