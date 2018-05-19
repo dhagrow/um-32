3c764d3d7022c34a45b3fc53e0718de5556ecb34
dhagrow <cymrow@gmail.com>
2018-05-19 19:48:53 -0400

    machine.lisp: actually reading correctly?!

--- CommonLisp/machine.lisp
+++ CommonLisp/machine.lisp
@@ -1,23 +1,14 @@
-(defconstant +buffer-size+ 4096)
-
 (defun read-uint32 (stream)
-  (loop repeat 4
-        for offset from 0 by 8
-        for value = (read-byte stream nil)
-          then (logior (ash (read-byte stream) offset) value)
-        finally (return value)))
+  (logior (ash (read-byte stream) 24)
+          (ash (read-byte stream) 16)
+          (ash (read-byte stream) 8)
+          (read-byte stream)))
 
 (defun load-um (filename)
-  (with-open-file (fp filename :direction :input
+  (with-open-file (stream filename :direction :input
                       :element-type '(unsigned-byte 8))
-    (loop for platter = (read-uint32 fp)
+    (loop for platter = (ignore-errors (read-uint32 stream))
           while platter
-          do (print platter))))
+          do (print (format nil "~32,'0b" platter)))))
 
-;; (load-um "scrolls/sandmark.umz")
-
-;; recursive-p ?
-(with-open-file (stream "docs/key.txt" :direction :input
-  (loop for line = (read-line stream nil nil)
-        while line
-        do (print line) )
+(load-um "scrolls/sandmark.umz")
