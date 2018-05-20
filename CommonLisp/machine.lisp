d26dc74782ec8fc58225c1d2c0141029c3e2f61f
dhagrow <cymrow@gmail.com>
2018-05-19 22:03:01 -0400

    machine.lisp looping program

--- CommonLisp/machine.lisp
+++ CommonLisp/machine.lisp
@@ -1,3 +1,6 @@
+(defvar program)
+(setq program '())
+
 (defun read-uint32 (stream)
   (logior (ash (read-byte stream) 24)
           (ash (read-byte stream) 16)
@@ -6,9 +9,16 @@
 
 (defun load-um (filename)
   (with-open-file (stream filename :direction :input
-                      :element-type '(unsigned-byte 8))
+                          :element-type '(unsigned-byte 8))
     (loop for platter = (ignore-errors (read-uint32 stream))
           while platter
-          do (print (format nil "~32,'0b" platter)))))
+          do (setq program (append program (list platter))))))
+
+(defun run ()
+  (loop for platter in program
+    do (format t "~32,'0b~%" platter)))
 
 (load-um "scrolls/sandmark.umz")
+(run)
+;; (print program)
+;; print (format nil "~32,'0b" platter))
