3c1db6d096eac237306779a58fa1615a360050da
miguel <cymrow@gmail.com>
2018-05-21 15:33:59 -0400

    machine.lisp: progress. DIV error

--- CommonLisp/machine.lisp
+++ CommonLisp/machine.lisp
@@ -19,28 +19,38 @@
           do (setf (nth 0 memory) (append (nth 0 memory) (list platter))))))
 
 (defun run ()
-  (loop for platter in (nth 0 memory)
-    for code = (ash platter -28)
-    do
-      (if (eql code 13) (let (
-        (a (logand (ash platter -25) 7))
-        (val (logand platter #x1ffffff)))
-        (progn
-          (format t "reg=~d~%" reg)
-          (format t "code=~d a=~d val=~d~%" code a val))
-          (setf (aref reg a) val))
-      (let (
-        (a (logand (ash platter -6) 7))
-        (b (logand (ash platter -3) 7))
-        (c (logand platter 7)))
-        (progn
-          (format t "reg=~d~%" reg)
-          (format t "code=~d a=~d b=~d c=~d~%" code a b c)
-          (ccase code
-          (0 (if
-            (not (eql (aref reg c) 0))
+  (let ((finger 0))
+    (loop
+      for platter = (nth finger (nth 0 memory))
+      for code = (ash platter -28)
+      do (progn
+        (if (eql code 13) (let (
+          (a (logand (ash platter -25) 7))
+          (val (logand platter #x1ffffff)))
+          (progn
+            ;; (format t "finger=~d reg=~d~%" finger reg)
+            ;; (format t "code=~d a=~d val=~d~%" code a val)
+            (setf (aref reg a) val)))
+        (let (
+          (a (logand (ash platter -6) 7))
+          (b (logand (ash platter -3) 7))
+          (c (logand platter 7)))
+          (progn
+            ;; (format t "finger=~d reg=~d~%" finger reg)
+            ;; (format t "code=~d a=~d b=~d c=~d~%" code a b c)
+            (ccase code
+            (0 (if (not (eql (aref reg c) 0))
               (setf (aref reg a) (aref reg b))))
-          (3 (setf (aref reg a) (+ (aref reg b) (aref reg c))))))))))
+            (3 (setf (aref reg a) (mod (+ (aref reg b) (aref reg c)) (expt 2 32))))
+            (4 (setf (aref reg a) (mod (* (aref reg b) (aref reg c)) (expt 2 32))))
+            (5 (setf (aref reg a) (floor (aref reg b) (aref reg c))))
+            (6 (setf (aref reg a) (boole boole-nand (aref reg b) (aref reg c))))
+            (10 (format t "~d" (code-char (aref reg c))))
+            (12 (progn
+              (if (not (eql (aref reg b) 0))
+                (setf (nth 0 memory) (copy-list (nth (aref reg b) memory))))
+              (setq finger (- (aref reg c) 1))))))))
+        (incf finger)))))
 
 (load-um "scrolls/sandmark.umz")
 (run)
