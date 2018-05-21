b7cc57d607a0817ec32e7c35a86d45eb4bbf8420
miguel <cymrow@gmail.com>
2018-05-21 16:04:12 -0400

    machine.lisp: unsigned int issue with nand

--- CommonLisp/machine.lisp
+++ CommonLisp/machine.lisp
@@ -2,7 +2,7 @@
 (defvar reg)
 
 (setf memory (list '()))
-(setf reg (make-array '(8) :initial-element 0))
+(setf reg (make-array '(8) :element-type '(unsigned-byte 32) :initial-element 0))
 
 (defun read-uint32 (stream)
   (logior (ash (read-byte stream) 24)
@@ -24,32 +24,30 @@
       for platter = (nth finger (nth 0 memory))
       for code = (ash platter -28)
       do (progn
-        (if (eql code 13) (let (
-          (a (logand (ash platter -25) 7))
-          (val (logand platter #x1ffffff)))
-          (progn
+        (if (eql code 13)
+          (let ((a (logand (ash platter -25) 7))
+                (val (logand platter #x1ffffff)))
             ;; (format t "finger=~d reg=~d~%" finger reg)
             ;; (format t "code=~d a=~d val=~d~%" code a val)
-            (setf (aref reg a) val)))
-        (let (
-          (a (logand (ash platter -6) 7))
-          (b (logand (ash platter -3) 7))
-          (c (logand platter 7)))
-          (progn
-            ;; (format t "finger=~d reg=~d~%" finger reg)
-            ;; (format t "code=~d a=~d b=~d c=~d~%" code a b c)
-            (ccase code
-            (0 (if (not (eql (aref reg c) 0))
-              (setf (aref reg a) (aref reg b))))
-            (3 (setf (aref reg a) (mod (+ (aref reg b) (aref reg c)) (expt 2 32))))
-            (4 (setf (aref reg a) (mod (* (aref reg b) (aref reg c)) (expt 2 32))))
-            (5 (setf (aref reg a) (floor (aref reg b) (aref reg c))))
-            (6 (setf (aref reg a) (boole boole-nand (aref reg b) (aref reg c))))
-            (10 (format t "~d" (code-char (aref reg c))))
-            (12 (progn
-              (if (not (eql (aref reg b) 0))
-                (setf (nth 0 memory) (copy-list (nth (aref reg b) memory))))
-              (setq finger (- (aref reg c) 1))))))))
+            (setf (aref reg a) val))
+        (let ((a (logand (ash platter -6) 7))
+              (b (logand (ash platter -3) 7))
+              (c (logand platter 7)))
+          (declare ((unsigned-byte 32) a b c))
+          (format t "finger=~d reg=~d~%" finger reg)
+          (format t "code=~d a=~d b=~d c=~d~%" code a b c)
+          (ccase code
+          (0 (if (not (eql (aref reg c) 0))
+            (setf (aref reg a) (aref reg b))))
+          (3 (setf (aref reg a) (mod (+ (aref reg b) (aref reg c)) (expt 2 32))))
+          (4 (setf (aref reg a) (mod (* (aref reg b) (aref reg c)) (expt 2 32))))
+          (5 (setf (aref reg a) (floor (aref reg b) (aref reg c))))
+          (6 (setf (aref reg a) (lognand (aref reg b) (aref reg c))))
+          (10 (format t "~d" (code-char (aref reg c))))
+          (12 (progn
+            (if (not (eql (aref reg b) 0))
+              (setf (nth 0 memory) (copy-list (nth (aref reg b) memory))))
+            (setq finger (- (aref reg c) 1)))))))
         (incf finger)))))
 
 (load-um "scrolls/sandmark.umz")
