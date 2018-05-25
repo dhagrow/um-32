4cbf38077ff491117b02268df6b0579cd1f75872
dhagrow <cymrow@gmail.com>
2018-05-24 23:16:30 -0400

    machine.lisp: optimized by changing all memory list->array

--- CommonLisp/machine.lisp
+++ CommonLisp/machine.lisp
@@ -3,7 +3,6 @@
 (defvar reg)
 (defvar op)
 
-(setf memory '(nil))
 (setf abandoned-indexes '())
 (setf reg (make-array '(8) :element-type '(unsigned-byte 32) :initial-element 0))
 (setq op (make-array 14 :initial-contents '(cmv aix aam add mul dvi nad hlt alc abd out inp lod ort)))
@@ -17,17 +16,22 @@
           (read-byte stream)))
 
 (defun load-um (filename)
-  (setf (nth 0 memory) '())
   (with-open-file (stream filename :direction :input
                           :element-type '(unsigned-byte 8))
-    (loop for platter = (ignore-errors (read-uint32 stream))
-          while platter
-          do (setf (nth 0 memory) (append (nth 0 memory) (list platter))))))
+    (let ((i 0)
+          (n (floor (file-length stream) 4)))
+      (setf memory (make-array 1))
+      (setf (aref memory 0) (make-array n))
+      (loop
+        for platter = (ignore-errors (read-uint32 stream))
+        while platter
+        do (setf (aref (aref memory 0) i) platter)
+          (incf i)))))
 
 (defun run ()
   (let ((finger 0))
     (loop
-      for platter = (nth finger (nth 0 memory))
+      for platter = (aref (aref memory 0) finger)
       for code = (ash platter -28)
       do (progn
         (if (eql code 13)
@@ -40,14 +44,14 @@
                 (b (logand (ash platter -3) 7))
                 (c (logand platter 7)))
             ;; (format t "finger=~d reg=~d~%" finger reg)
-            ;; (format t "code=~d a=~d b=~d c=~d~%" code a b c)
+            ;; (format t "code=~d a=~d b=~d c=~d~%" (aref op code) a b c)
             (ccase (aref op code)
             (cmv (if (not (eql (aref reg c) 0))
                      (setf (aref reg a) (aref reg b))))
             (aix (setf (aref reg a)
-                       (nth (aref reg c) (nth (aref reg b) memory))))
-            (aam (setf (nth (aref reg b)
-                       (nth (aref reg a) memory)) (aref reg c)))
+                       (aref (aref memory (aref reg b)) (aref reg c))))
+            (aam (setf (aref (aref memory (aref reg a)) (aref reg b))
+                       (aref reg c)))
             (add (setf (aref reg a)
                        (uint32 (+ (aref reg b) (aref reg c)))))
             (mul (setf (aref reg a)
@@ -56,17 +60,20 @@
             (nad (setf (aref reg a)
                        (uint32 (lognand (aref reg b) (aref reg c)))))
             (alc (let ((index (pop abandoned-indexes))
-                      (new-list (make-list (aref reg c) :initial-element 0)))
+                       (new-array (make-array (aref reg c) :initial-element 0)))
                   (if (not (eql index nil))
-                      (setf (nth index memory) new-list)
-                      (setf memory (append memory (list new-list))))
-                  (setf (aref reg b) (or index (- (list-length memory) 1)))))
-            (abd (progn (setf (nth (aref reg c) memory) nil)
+                      (setf (aref memory index) new-array)
+                      (progn
+                        (setq index (length memory))
+                        (setf memory (adjust-array memory (+ index 1)))
+                        (setf (aref memory index) new-array)))
+                  (setf (aref reg b) index)))
+            (abd (progn (setf (aref memory (aref reg c)) nil)
                         (push (aref reg c) abandoned-indexes)))
             (out (princ (code-char (aref reg c))) (force-output))
             (lod (progn
               (if (not (eql (aref reg b) 0))
-                (setf (nth 0 memory) (copy-list (nth (aref reg b) memory))))
+                (setf (aref memory 0) (copy-seq (aref memory (aref reg b)))))
               (setq finger (- (aref reg c) 1)))))))
         (incf finger)))))
 
