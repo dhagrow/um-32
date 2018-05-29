5d30219478cbee28075dca285846d5539fa4debc
dhagrow <cymrow@gmail.com>
2018-05-28 23:34:19 -0400

    machine.d: progress

--- D/machine.d
+++ D/machine.d
@@ -1,11 +1,13 @@
 import std.conv, std.stdio, std.stdint, std.bitmanip, std.format, std.container;
 
 auto memory = new uint32_t[][](0, 0);
+uint32_t[8] reg;
 
 enum Op {cmv, aix, aam, add, mul, dvi, nad, hlt, alc, abd, otp, inp, lod, ort};
 
 void run() {
     bool stop = false;
+    auto cycle = 0;
     uint32_t finger = 0;
     uint32_t platter = 0;
     Op op;
@@ -20,23 +22,42 @@ void run() {
         code = (platter >> 28);
         op = to!Op(code);
 
+        // writefln("cycle: %s finger: %s reg: %s", cycle, finger, reg);
+
         if (op == Op.ort) {
             a = (platter >> 25) & 7;
             val = platter & 0x1ffffff;
-            writefln("%s(%s, %s)", op, a, val);
+            // writefln("%s(%s, %s)", op, a, val);
+
+            reg[a] = val;
         } else {
             a = (platter >> 6) & 7;
             b = (platter >> 3) & 7;
             c = platter & 7;
-            writefln("%s(%s, %s, %s)", op, a, b, c);
+            // writefln("%s(%s, %s, %s)", op, a, b, c);
 
             switch (op) {
+                case Op.cmv:
+                    if (reg[c] != 0) {
+                        reg[a] = reg[b];
+                    }
+                    break;
+                case Op.add:
+                    reg[a] = (reg[b] + reg[c]) % (1UL << 32);
+                    break;
+                case Op.lod:
+                    if (reg[b] != 0) {
+                        memory[0] = memory[reg[b]];
+                    }
+                    finger = reg[c] - 1;
+                    break;
                 default:
                     throw new Exception(format("unknown op: %s", op));
             }
         }
 
         ++finger;
+        ++cycle;
     }
 }
 
