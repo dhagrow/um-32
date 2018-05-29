d48eaa9167e2a74955b8fc03973f8fb84e4d99ed
dhagrow <cymrow@gmail.com>
2018-05-28 23:13:59 -0400

    machine.d: boom

--- D/machine.d
+++ D/machine.d
@@ -1,8 +1,57 @@
-import std.stdio, std.stdint;
+import std.conv, std.stdio, std.stdint, std.format, std.container;
+
+auto memory = new uint32_t[][](0, 0);
+
+enum Op {cmv, aix, aam, add, mul, dvi, nad, hlt, alc, abd, otp, inp, lod, ort};
+
+void run() {
+    bool stop = false;
+    uint32_t finger = 0;
+    uint32_t platter = 0;
+    Op op;
+    uint8_t code = 0;
+    uint8_t a = 0;
+    uint8_t b = 0;
+    uint8_t c = 0;
+    uint32_t val = 0;
+
+    while (!stop) {
+        platter = memory[0][finger];
+        code = (platter >> 28);
+        op = to!Op(code);
+
+        if (op == Op.ort) {
+            a = (platter >> 25) & 7;
+            val = platter & 0x1ffffff;
+            writefln("%s(%s, %s)", op, a, val);
+        } else {
+            a = (platter >> 6) & 7;
+            b = (platter >> 3) & 7;
+            c = platter & 7;
+            writefln("%s(%s, %s, %s)", op, a, b, c);
+
+            switch (op) {
+                default:
+                    throw new Exception(format("unknown op: %s", op));
+            }
+        }
+
+        ++finger;
+    }
+}
+
+void load() {
+    memory.length = 0;
+    memory.length = 1;
+
+    File f = File("scrolls/sandmark.umz");
+    while (!f.eof()) {
+        auto platter = *cast(uint32_t*) f.rawRead(new ubyte[4]);
+        memory[0] ~= platter;
+    }
+}
 
 void main() {
-    File f = File("scrolls/sandmark.umz");
-    auto buf = f.rawRead(new ubyte[4]);
-    auto platter = *cast(uint32_t*) buf;
-    platter.writeln;
+    load();
+    run();
 }
