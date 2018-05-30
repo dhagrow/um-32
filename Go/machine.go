4c26e7d2e3a47eca139cceb46d18dec4a61985ab
dhagrow <cymrow@gmail.com>
2018-05-30 00:29:12 -0400

    machine.go: progress

--- Go/machine.go
+++ Go/machine.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+    "io"
+	"fmt"
+	"log"
+    "os"
+    "bytes"
+    "encoding/binary"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	path := "scrolls/sandmark.umz"
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal("Error while opening file", err)
+    }
+
+    chunk := make([]byte, 4)
+    var platter uint32
+
+    for {
+        _, err = file.Read(chunk)
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            log.Fatal(err)
+        }
+
+        buf := bytes.NewReader(chunk)
+        binary.Read(buf, binary.BigEndian, &platter)
+
+        fmt.Printf("platter: %d\n", platter)
+    }
+
+	defer file.Close()
+
+	fmt.Printf("%s opened\n", path)
 }
